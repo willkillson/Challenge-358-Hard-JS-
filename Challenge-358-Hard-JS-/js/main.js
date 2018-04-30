1f26bee43ad49b17c69d43fccbfa7baa285b60da
kevin wilkinson <willkillson@gmail.com>
2018-04-30 06:31:00 -0700

    Created new proj

--- Challenge-358-Hard-JS-/js/main.js
+++ Challenge-358-Hard-JS-/js/main.js
@@ -1,1 +1,63 @@
-﻿// Your code here!
+﻿
+
+
+
+var resultOne = null;
+var fileInputOne = document.getElementById('input');
+var rawBtn = document.getElementById('DisplayRaw');
+var processedBtn = document.getElementById('DisplayProcessed');
+
+
+function processFunction(r) {
+
+    return "Not completed"
+}
+
+
+
+window.onload = function () {
+    fileInputOne.addEventListener('change', function (e) {
+        var file = fileInputOne.files[0];
+        var textType = /text.*/;
+
+        if (file.type.match(textType)) {
+            var reader = new FileReader();
+
+
+            reader.onload = function (e) {
+                resultOne = reader.result;
+                console.log("resultOne loaded");
+            }
+            reader.readAsText(file);
+        }
+        else {
+            fileDisplayArea.innerText = "File not supported!"
+        }
+
+
+
+    });
+
+}
+
+processedBtn.onclick = function () {
+    let display_p = document.getElementById('processedInput');
+
+    if ((resultOne === null)) {
+        display_p.innerText = "no input";
+    }
+    else {
+        display_p.innerText = processFunction(resultOne);
+    }
+};
+rawBtn.onclick = function () {
+    let display_p = document.getElementById('unprocessedInput');
+
+    if (resultOne !== null) {
+        display_p.innerText = resultOne;
+    }
+    else {
+        display_p.innerText = "no input";
+    }
+}
+
